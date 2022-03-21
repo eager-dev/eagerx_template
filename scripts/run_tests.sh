4b81473b8c377f1eb7aeac3455f9041be1bf8866
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2022-03-21 12:49:36 +0100

    ci: fix pytest argument

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-poetry run pytest --cov-config .coveragerc --source ${PACKAGE_NAME} --cov-report html --cov-report xml --cov-report term --cov=. -v --color=yes
+poetry run pytest --cov-config .coveragerc --rootdir=${PACKAGE_NAME} --cov-report html --cov-report xml --cov-report term --cov=. -v --color=yes
